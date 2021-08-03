facb93a19ed5ae67ade954d100bd8fa43ba4dea1
jiho lee <optional.int@kakao.com>
2021-08-04 08:18:15 +0900

    fix: get_ip_address

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,6 @@
 def get_ip_address():
-    import socket
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.connect(("8.8.8.8", 80))
-        ip_addr = sock.getsockname()[0]
-    return ip_addr
+    from requests import get
+    return get('https://ipapi.co/ip/').text
 
 ip_address = get_ip_address()
 webserver_port = 8087
