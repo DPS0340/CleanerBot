d5b170038a830111c96cfee3968923abdf78bb78
jiho lee <optional.int@kakao.com>
2022-02-06 16:41:30 +0900

    fix: change host manager ip to 127.0.0.1

--- host-manager/index.ts
+++ host-manager/index.ts
@@ -1,6 +1,6 @@
 import * as hostile from "./hostile.js";
 
-const ip = "13.125.8.153";
+const ip = "127.0.0.1";
 
 hostile.set(ip, "cleanerbot.dcinside.com");
 console.log("cleanerbot.dcinside.com -> 봇 서버 IP DNS 등록 추가!");
