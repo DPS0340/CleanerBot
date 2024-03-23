b116425f78c1680b4931b923bfafb0e99846e224
Jiho Lee <optional.int@kakao.com>
2024-03-23 14:53:32 +0900

    feat: slash command integration

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import asyncio
 import logging
 from discord.ext import commands
-from discord import Embed
+from discord import Embed, Interaction
+from discord import app_commands
 import discord
 from server import ArcaProxyServer, GallogProxyServer, LoginProxyServer
 import cleaner
@@ -10,132 +11,145 @@ from log import logger
 from constants import ip_address
 
 
-prefix = "clb "
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix))
+intents = discord.Intents.default()
+intents.message_content = True
+
+prefix = "/"
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 token = get_token()
 
 auths = dict()
 
 
-@bot.event
-async def on_ready():
-    logging.info(f"bot ip address: {ip_address}")
-    logger.info('Logged on as {0}!'.format(bot.user))
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#         return
+#     mention = f"<@!{bot.user.id}>"
+#     if mention in message.content:
+#         await help(message)
+#         return
+#     if not message.content.startswith(prefix):
+#         return
+#     if message.guild:
+#         await message.channel.send("개인정보 유출을 방지하기 위해 DM으로 해주세요!")
+#         await message.channel.send(f"헬프는 {mention} 멘션")
+#     await bot.process_commands(message)
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    mention = f"<@!{bot.user.id}>"
-    if mention in message.content:
-        await help(message)
-        return
-    if not message.content.startswith(prefix):
-        return
-    if message.guild:
-        await message.channel.send("개인정보 유출을 방지하기 위해 DM으로 해주세요!")
-        await message.channel.send(f"헬프는 {mention} 멘션")
-    await bot.process_commands(message)
-
-
-async def help(message):
-    embed = Embed(title="CLEANERBOT 사용 설명서", color=0x95e4fe)
+@bot.tree.command(name="help", description="help")
+async def help_(ctx: Interaction):
+    embed = Embed(title="CLEANERBOT 사용 설명서", color=0x95E4FE)
 
     embed.add_field(
-        name=f"{prefix}login id pw", value="id와 pw를 통해 로그인합니다.", inline=False)
+        name=f"{prefix}login id pw", value="id와 pw를 통해 로그인합니다.", inline=False
+    )
     embed.add_field(
-        name="제한 사항", value="이후 커맨드는 로그인된 사용자만 사용 가능합니다.", inline=True)
-    embed.add_field(name=f"{prefix}stat",
-                    value="글과 댓글 갯수를 보여줍니다.", inline=False)
+        name="제한 사항",
+        value="이후 커맨드는 로그인된 사용자만 사용 가능합니다.",
+        inline=True,
+    )
+    embed.add_field(
+        name=f"{prefix}stat", value="글과 댓글 갯수를 보여줍니다.", inline=False
+    )
     embed.add_field(name=f"{prefix}clean", value="글과 댓글을 지웁니다.", inline=False)
     embed.add_field(name=f"{prefix}post", value="글을 지웁니다.", inline=False)
     embed.add_field(name=f"{prefix}comment", value="댓글을 지웁니다.", inline=False)
-    embed.add_field(name=f"{prefix}arca id pw nickname",
-                    value="id와 pw, 닉네임을 통해 아카라이브에 있는 글과 댓글을 지웁니다.", inline=False)
-    embed.add_field(name=f"{prefix}arca post id pw nickname",
-                    value="id와 pw, 닉네임을 통해 아카라이브에 있는 글을 지웁니다.", inline=False)
-    embed.add_field(name=f"{prefix}arca comment id pw nickname",
-                    value="id와 pw, 닉네임을 통해 아카라이브에 있는 댓글을 지웁니다.", inline=False)
     embed.add_field(
-        name=f"Github", value="https://github.com/DPS0340/CleanerBot", inline=False)
+        name=f"{prefix}arca id pw nickname",
+        value="id와 pw, 닉네임을 통해 아카라이브에 있는 글과 댓글을 지웁니다.",
+        inline=False,
+    )
+    embed.add_field(
+        name=f"{prefix}arca post id pw nickname",
+        value="id와 pw, 닉네임을 통해 아카라이브에 있는 글을 지웁니다.",
+        inline=False,
+    )
+    embed.add_field(
+        name=f"{prefix}arca comment id pw nickname",
+        value="id와 pw, 닉네임을 통해 아카라이브에 있는 댓글을 지웁니다.",
+        inline=False,
+    )
+    embed.add_field(
+        name=f"Github", value="https://github.com/DPS0340/CleanerBot", inline=False
+    )
 
-    await message.channel.send(embed=embed)
+    await ctx.response.send_message(embed=embed)
 
 
-@bot.command()
-async def login(ctx: commands.Context, id, pw):
-    message = ctx.message
+@bot.tree.command(name="login", description="login")
+async def login(ctx: Interaction, id: str, pw: str):
     if not (id and pw):
-        await message.channel.send("잘못된 인자입니다!")
+        await ctx.response.send_message("잘못된 인자입니다!")
         return
-    auths[ctx.author.id] = {'id': id, 'pw': pw}
-    await message.channel.send("로그인이 완료되었습니다!")
+    auths[ctx.user.id] = {"id": id, "pw": pw}
+    await ctx.response.send_message("로그인이 완료되었습니다!")
 
 
-async def invokeClean(ctx: commands.Context, posting=True, comment=True):
-    message = ctx.message
-    uid = ctx.author.id
+async def invokeClean(ctx: Interaction, posting=True, comment=True):
+    uid = ctx.user.id
     if not uid in auths:
-        await message.channel.send("로그인 해주세요!")
+        await ctx.response.send_message("로그인 해주세요!")
         return
-    await message.channel.send("삭제중..")
+    await ctx.response.send_message("삭제중..")
     auth = auths[uid]
     await cleanMatchArg(ctx, auth, posting, comment)
 
 
-async def cleanMatchArg(ctx: commands.Context, auth, posting=True, comment=True):
+async def cleanMatchArg(ctx: Interaction, auth, posting=True, comment=True):
     await cleaner.loginAndClean(bot, ctx, auth, posting, comment)
 
 
-@bot.command()
-async def clean(ctx: commands.Context):
+@bot.tree.command(name="clean", description="clean")
+async def clean(ctx: Interaction):
     await invokeClean(ctx, True, True)
 
 
-@bot.command()
-async def post(ctx: commands.Context):
+@bot.tree.command(name="post", description="post")
+async def post(ctx: Interaction):
     await invokeClean(ctx, True, False)
 
 
-@bot.command()
-async def comment(ctx: commands.Context):
+@bot.tree.command(name="comment", description="comment")
+async def comment(ctx: Interaction):
     await invokeClean(ctx, False, True)
 
 
-@bot.command()
-async def arca(ctx: commands.Context, *args):
-    message = ctx.message
-    if len(args) == 3:
-        id, pw, nickname = args
-        flag = ''
-    elif len(args) == 4:
-        flag, id, pw, nickname = args
-    else:
-        await message.channel.send("잘못된 인자입니다!")
-        return
+@bot.tree.command(name="arca", description="arca")
+async def arca(
+    ctx: Interaction,
+    flag: str,
+    id: str,
+    pw: str,
+    nickname: str,
+):
     posting = True
     comment = True
-    if flag == 'post':
+    if flag == "post":
         comment = False
-    elif flag == 'comment':
+    elif flag == "comment":
         posting = False
-    elif flag != '':
-        await message.channel.send("잘못된 인자입니다!")
+    elif flag != "all":
+        await ctx.response.send_message("잘못된 인자입니다!")
         return
     await cleaner.cleanArcaLive(bot, ctx, id, pw, nickname, posting, comment)
 
 
-@bot.command()
-async def stat(ctx: commands.Context):
-    message = ctx.message
-    uid = ctx.author.id
+@bot.tree.command(name="stat", description="stat")
+async def stat(ctx: Interaction):
+    uid = ctx.user.id
     if not uid in auths:
-        await message.channel.send("로그인 해주세요!")
+        await ctx.response.send_message("로그인 해주세요!")
         return
     auth = auths[uid]
-    nickname, post_num, comment_num = await asyncio.gather(cleaner.get_nickname(auth), cleaner.get_num(auth, _type='posting'), cleaner.get_num(auth, _type='comment'))
-    await message.channel.send(f"{nickname}님은 글 {post_num}개와 댓글 {comment_num}개를 작성하셨습니다.")
+    nickname, post_num, comment_num = await asyncio.gather(
+        cleaner.get_nickname(auth),
+        cleaner.get_num(auth, _type="posting"),
+        cleaner.get_num(auth, _type="comment"),
+    )
+    await ctx.response.send_message(
+        f"{nickname}님은 글 {post_num}개와 댓글 {comment_num}개를 작성하셨습니다."
+    )
 
 
 def matchPrefix(message):
@@ -146,8 +160,23 @@ def isDM(message):
     return not message.guild
 
 
-if __name__ == '__main__':
-    bot.add_cog(ArcaProxyServer(bot))
-    bot.add_cog(GallogProxyServer(bot))
-    bot.add_cog(LoginProxyServer(bot))
+@bot.event
+async def setup_hook():
+    await bot.add_cog(ArcaProxyServer(bot))
+    await bot.add_cog(GallogProxyServer(bot))
+    await bot.add_cog(LoginProxyServer(bot))
+
+
+@bot.event
+async def on_ready():
+    logger.info(f"bot ip address: {ip_address}")
+    logger.info("Logged on as {0}!".format(bot.user))
+    logger.info(await bot.tree.sync())
+    for server in bot.guilds:
+        logger.info(server)
+        logger.info(server.id)
+        logger.info(await bot.tree.sync(guild=discord.Object(id=server.id)))
+
+
+if __name__ == "__main__":
     bot.run(token)
